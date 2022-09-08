057401cc5d019360a21acf3b958116b25a1dd85c
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2022-09-08 17:04:19 -0600

    code to explore integer precision

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
-﻿int a = 18;
+﻿WorkingWithIntegers();
+OderPrecedence();
+void WorkingWithIntegers()
+{
+int a = 18;
 int b = 6;
 int c = a + b;
+// subtraction
 Console.WriteLine(c);
 c = a-b;
+//multiplication
 Console.WriteLine(c);
 c= a*b;
+// division
 Console.WriteLine (c);
 c= a/b;
 Console.WriteLine(c);
-c= a+b -12 *17;
-Console.WriteLine(c);
+}
+
+void OderPrecedence()
+{
+int a=5;
+int b=4;
+int c=2;
+int d=a+b*c;
+Console.WriteLine(d);
+d=(a+b)*c;
+Console.WriteLine(d);
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+int e=7;
+int f=4;
+int g=3;
+int h= (e+f)/ g;
+Console.WriteLine(h);
+}
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+int max= int.MaxValue;
+int min= int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
